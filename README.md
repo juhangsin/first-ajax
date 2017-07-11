31c6a6c0605dc65d647bbed948af10965195813e
Ju-Hang Sin <juhangsin@gmail.com>
2017-07-11 13:21:48 -0400

    COMEPELTED MF

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # Your First (Several) Asynchronous Requests
 
 [See assignment in Alexa.](https://alexa.bitmaker.co/cohorts/67/assignments/2055/latest)
+
+Step 0
+
+What HTTP method did your browser use to make the request?
+- GET
+
+How many milliseconds did it take your browser to complete it?
+- 39ms
+
+What HTTP status code did the server return? What does that mean?
+- 200 OK - The request has succeeded
+
+Look at the "Initiator" and "Type" columns. Was this an xhr/JavaScript/AJAX request or a normal browser request?
+- Not AJAX
+
+
+Step 1
+
+What are the values in the "Method" and "Status" columns? Compare these values to Step 0.
+- Status: 200 OK, Method: GET
+
+What are the values in the "Type" and "Initiator" columns? Compare these values to Step 0.
+- Type: xhr, Initiator: script
--- ajax.js
+++ ajax.js
@@ -1,5 +1,102 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  /* Your code goes here */
+	var ajaxRequest = document.getElementById( 'ajax_request' );
+	var toPingPong  = document.getElementById( 'to_ping_pong');
+	var toCount     = document.getElementById( 'to_count' );
+	var toTime      = document.getElementById( 'to_time' );
+	var toACar      = document.getElementById( 'to_a_car' );
+
+
+	ajaxRequest.addEventListener( 'click', function() {
+		console.log( 'CLICKED AJAX REQUEST BUTTON');
+
+	$.ajax( {
+		url: 'http://first-ajax-api.herokuapp.com/',
+		method: 'GET',
+		data: {},
+		dataType: 'text',
+	});
+	})
+
+
+	toPingPong.addEventListener( 'click', function () {
+		console.log( 'CLICKED AJAX REQUEST TO PING/PONG');
+
+		$.ajax( {
+			url: 'http://first-ajax-api.herokuapp.com/ping',
+			method: 'GET',
+			dataType: 'text',
+		}).done( function(responseData) {
+			console.log('PINGPONG RESONPSE DATA');
+			console.log(responseData);
+			var element = document.createElement('div');
+			element.innerHTML = responseData;
+			document.querySelector( '#step3456' ).append( element );
+		}).fail( function( jqXHR, textStatus, errorThrown ) {
+			console.log( 'DIDN\'T WORK ');
+			console.log( textStatus, errorThrown );
+			var fail = document.createElement('div');
+			fail.innerHTML = "WE COULDN'T MAKE IT WORK, COME BACK LATER";
+			document.querySelector( '#step3456' ).append( fail);
+		}).always( function() {
+			console.log( 'alvvayssss' );
+			var always = document.createElement('div');
+			always.innerHTML = "Hey the request finished!";
+			document.querySelector( '#step3456' ).append(always);
+		});
+	});
+
+
+	toCount.addEventListener( 'click', function () {
+		console.log( 'Run AJAX Request to Count');
+
+		$.ajax({
+			url: 'http://first-ajax-api.herokuapp.com/count',
+			method: 'GET',
+			dataType: 'text',
+		}).done ( function(responseData) {
+			console.log('Count responseData');
+			console.log(responseData);
+			var element = document.createElement('div');
+			element.innerHTML = responseData;
+			document.querySelector( '#step7' ).append( element );
+		})
+	});
+
+
+	toTime.addEventListener( 'click', function () {
+		console.log( 'Running AJAX Request to Time' );
+
+		$.ajax({
+			url: 'http://first-ajax-api.herokuapp.com/time',
+			method: 'GET',
+			dataType: 'text',
+			data: {
+				timezone: 'America/Mexico_City',
+			}
+			 }).done (function(responseData) {
+			console.log('Count responseData');
+			console.log(responseData);
+			var element = document.createElement('div');
+			element.innerHTML = responseData;
+			document.querySelector( '#step8' ).append( element );
+		})
+	})
+
+	toACar.addEventListener( 'click', function () {
+		console.log( 'Run AJAX Request to A Car' );
+
+		$.ajax({
+			url: 'http://first-ajax-api.herokuapp.com/a_car',
+			method: 'GET',
+			dataType: 'html',
+		}).done (function(responseData) {
+			console.log( 'Count responseData' );
+			console.log( responseData );
+			var unorderedList = document.createElement( 'ul' );
+			unorderedList.innerHTML = responseData;
+			document.querySelector( '#step9' ).append( unorderedList );
+		})
+	})
 
 });
